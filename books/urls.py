aee14d83ea40f4a8b15fda42654c48a88836147a
khaydarr <khaydartursunov@gmail.com>
2023-07-03 21:44:13 +0500

    Books site

---
+++ books/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from .views import BookListCreateAPIView, BookUpdateDeleteAPIView, BookListAPIView, BookCreateAPIView, BookDetailAPIView, \
+    BookDeleteAPIView, BookUpdateAPIView, BookViewset
+
+router = SimpleRouter()
+router.register('books', BookViewset, basename='books')
+
+urlpatterns = [
+    # path('books/', BookListAPIView.as_view(), ),
+    # path('booklistcreate/', BookListCreateAPIView.as_view()),
+    # path('bookupdatedelete/<int:pk>/', BookUpdateDeleteAPIView.as_view()),
+    # path('books/create/', BookCreateAPIView.as_view()),
+    # path('books/<int:pk>/', BookDetailAPIView.as_view(),),
+    # path('books/<int:pk>/delete/', BookDeleteAPIView.as_view()),
+    # path('books/<int:pk>/update/', BookUpdateAPIView.as_view()),
+]
+
+urlpatterns += router.urls
